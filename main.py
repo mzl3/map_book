1b6448bfb263cc221c6cf0ec23fca0920ffb25db
mzl3 <166690441+mzl3@users.noreply.github.com>
2024-04-18 09:27:15 +0200

    wip: add menu

--- main.py
+++ main.py
@@ -11,4 +11,15 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajomy {user["name"]} opublikował: {user['posts']}")
 
-show_users(users)
+
+if __name__ == "__main__":
+    print("Witaj użytkowniku")
+    while True:
+        print("Menu:")
+        print("1.Wyświetl co u znajomych")
+        menu_option: str = input("Dokonaj wyboru:")
+        if menu_option == "0":
+            print("Program kończy pracę")
+            break
+        if menu_option == "1":
+            show_users(users)
