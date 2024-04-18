a72ce8211da336c21e24bb52ee432a949fd348de
mzl3 <166690441+mzl3@users.noreply.github.com>
2024-04-18 09:14:14 +0200

    wip: add a show_users function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 users: list[dict] = [
     {"name": "Dawid", "surname": "Bałuka", "posts": 6000},
-{"name": "Kewin", "surname": "Czajkowski", "posts": 6002},
-{"name": "Kamil", "surname": "Gil", "posts": 1_000_000},
-{"name": "Daniel", "surname": "Błaszczyk", "posts": 6},
+    {"name": "Kewin", "surname": "Czajkowski", "posts": 6002},
+    {"name": "Kamil", "surname": "Gil", "posts": 1_000_000},
+    {"name": "Daniel", "surname": "Błaszczyk", "posts": 6},
 
 ]
 
-for user in users:
-    print(f"Twój znajomy {user["name"]} opublikował: {user['posts']}")
 
+def show_users(user_list: list[dict]) -> None:
+    for user in user_list:
+        print(f"Twój znajomy {user["name"]} opublikował: {user['posts']}")
 
+show_users(users)
