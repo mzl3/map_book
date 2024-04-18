f36f411b537c11cc45d410448235dbcd7978194d
mzl3 <166690441+mzl3@users.noreply.github.com>
2024-04-18 11:20:28 +0200

    wip: add remove user function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.data import users
-from utils.crud import show_users
+from utils.crud import show_users, add_new_user, search_user
 
 
 if __name__ == "__main__":
@@ -7,9 +7,19 @@ if __name__ == "__main__":
     while True:
         print("Menu:")
         print("1.Wyświetl co u znajomych")
+        print("2.Dodaj użytkownika")
+        print("3.Szukaj uzytkownika")
+        print("4.Usun uzytkownika")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)
+        if menu_option=="3":
+            search_user(users)
+        if menu_option=='4':
+            remove_user(users)
+
--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,24 @@
-def show_users(user_list: list[dict]) ->None:
+def show_users(user_list: list[dict]) -> None:
     for user in user_list:
-        print(f"Twój znajomy {user ['name']} opublikował: {user['posts']}")
+        print(f"Twój znajomy {user['name']} opublikował: {user['posts']}")
+
+
+def add_new_user(users: list) -> None:
+    new_name: str = input("Enter your name: ")
+    new_surname: str = input("Enter your surname: ")
+    new_posts: int = int(input("Enter your posts: "))
+    new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
+    users.append(new_user)
+
+
+def search_user(users: list) -> None:
+    kogo_szukasz = input("Kogo szukasz?: ")
+    for user in users:
+        if user['name'] == kogo_szukasz:
+            print(user)
+
+def remove_user(users)->None:
+    kogo_szukasz=input("Kogo szukasz?: ")
+    for user in users:
+        if user['name']==kogo_szukasz:
+            users.remove(user)
