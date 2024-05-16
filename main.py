e9a50f5e472f12397236e4ffcf107fd21b8647d0
mzl3 <166690441+mzl3@users.noreply.github.com>
2024-05-16 08:39:43 +0200

    wip: add update user function

--- main.py
+++ main.py
@@ -7,9 +7,21 @@ if __name__ == "__main__":
     while True:
         print("Menu:")
         print("1.Wyświetl co u znajomych")
+        print("2.")
+        print("3.")
+        print("4.")
+        print("5.")
+        print("0.Zamknij program")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "1":
+            show_users(users)
+        if menu_option == "1":
+            show_users(users)
+        if menu_option == "1":
+            show_users(users)
+
